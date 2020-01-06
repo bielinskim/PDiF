163b67e7bda9b6f25c11653c863fe2adb0ea93f9
bielinskim <45104530+bielinskim@users.noreply.github.com>
2020-01-06 23:21:32 +0100

    Add files via upload

---
+++ Lab02.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+//==== Zad 01 ====
+let list = 25::14::4::7::[]
+
+let rec aritMean total i list = 
+  match list with
+  | [] -> total / i
+  | x::xs -> 
+     let ntotal = x + total
+     aritMean ntotal (i+1) xs
+             
+aritMean 0 0 list
+
+//==== Zad 02 ====
+let list02 = 15::-33::44::-7::53::-25::[]
+
+let rec findMinMax min max list02 =
+  match list02 with
+  | [] -> (min, max)
+  | x::xs -> 
+     if min = 0 && max = 0 then findMinMax x x xs
+     elif x > max then findMinMax min x xs
+     elif x < min then findMinMax x max xs
+     else findMinMax min max xs
+
+   
+findMinMax 0 0 list02
